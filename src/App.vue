07bec98d413511d95f0caa34563843b8ff2d8ee3
Guilherme Oliveira dos Santos <guilhermeolsnto@gmail.com>
2024-01-21 11:00:07 -0300

    listas

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { reactive } from 'vue';
+import { reactive } from "vue";
 
 const nome = "guilherme";
 const meuObj = {
@@ -12,7 +12,7 @@ function dizOla() {
 }
 
 const imagemSteve =
-"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYF0zwdx2FqYIQgkJebwpFjmgcDnZVSftqfsyT5_-IFA&s";
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYF0zwdx2FqYIQgkJebwpFjmgcDnZVSftqfsyT5_-IFA&s";
 
 const imagemBruce =
   "https://s2-g1.glbimg.com/_2yHi4edVB_we_QTDhaTaY2w_-E=/0x0:1700x1065/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2022/T/B/uVGugSSFirAUUDgxGV5g/dsc-0555.jpg";
@@ -27,10 +27,12 @@ const estaAutorizado = true;
 // let contador = 0
 const estado = reactive({
   contador: 0,
-  email: '',
+  email: "",
   saldo: 5000,
   transferindo: 0,
-})
+  nomes: ["guilherme", "steve", "bruce"],
+  nomeAInserir: ','
+});
 
 function incrementar() {
   estado.contador++;
@@ -45,13 +47,21 @@ function alteraEmail(evento) {
 }
 
 function mostraSaldoFuturo() {
-  const {saldo, transferindo } = estado;
+  const { saldo, transferindo } = estado;
   return saldo - transferindo;
 }
 
 function validadoValorTransferencia() {
-  const {saldo, transferindo } = estado;
+  const { saldo, transferindo } = estado;
   return saldo >= transferindo;
+}
+
+function cadastraNome() {
+  if (estado.nomeAInserir.length >= 3) {
+    estado.nomes.push(estado.nomeAInserir)
+  } else {
+    alert("Digite mais caracteres")
+  }
 }
 
 </script>
@@ -67,34 +77,50 @@ function validadoValorTransferencia() {
 
   <button :disabled="!botaoEstaDesabilitado">enviar mensagem</button>
 
-  <br>
-  <hr>
+  <br />
+  <hr />
 
-  {{estado.contador}}
+  {{ estado.contador }}
 
-  <button @click="incrementar"  type="button">+</button>
-  <button @click="decrementar"  type="button">-</button>
+  <button @click="incrementar" type="button">+</button>
+  <button @click="decrementar" type="button">-</button>
 
+  <br />
+  <hr />
 
-  <br>
-  <hr>
- 
-  {{ estado.email}}
-  <input type="email" @keyup="alteraEmail">
+  {{ estado.email }}
+  <input type="email" @keyup="alteraEmail" />
 
-  <br>
-  <hr>
- 
-  Saldo: {{ estado.saldo }} <br>
-  Transferindo:{{ estado.transferindo }} <br>
-  Saldo depois da tranferência: {{ mostraSaldoFuturo() }} <br>
-  <input class="campo"  :class="{invalido: !validadoValorTransferencia()}"  @keyup="evento => estado.transferindo = evento.target.value"  type="number" placeholder="Quantia para transferir">
+  <br />
+  <hr />
+
+  Saldo: {{ estado.saldo }} <br />
+  Transferindo:{{ estado.transferindo }} <br />
+  Saldo depois da tranferência: {{ mostraSaldoFuturo() }} <br />
+  <input
+    class="campo"
+    :class="{ invalido: !validadoValorTransferencia() }"
+    @keyup="(evento) => (estado.transferindo = evento.target.value)"
+    type="number"
+    placeholder="Quantia para transferir"
+  />
   <button v-if="validadoValorTransferencia()">Tranferir</button>
   <span v-else>Valor maior que o saldo</span>
 
+  <br />
+  <hr />
+
+  <ul>
+    <li v-for="nome in estado.nomes">
+      {{ nome }}
+    </li>
+  </ul>
+
+  <input @keyup="evento => estado.nomeAInserir = evento.target.value"  type="text" placeholder="Digite um novo nome">
+  <button @click="cadastraNome"  type="button">Cadastrar nome</button>
+
+  <h3 v-for="nome in estado.nomes">{{ nome }}</h3>
 </template>
-
-
 
 <style scoped>
 img {
